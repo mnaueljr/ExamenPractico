dd671ea28fdc043350e38d3323998dff4a773ca9
mnaueljr <manuel.coj.villa@gmail.com>
2023-11-28 09:26:33 -0600

    Archivos Subidos

---
+++ main.py
@@ -0,0 +1,10 @@
+nombre = input("Ingrese Su Nombre: ")
+cantidad = input("Ingrese la cantidad de Pantalon a Llevar: ")
+if cantidad > 7 then
+mensaje("Tiene un descuento de 10%:");
+elseif
+cantidad > 10
+then
+mensaje("Tiene un Descuento de 25%:");
+else
+mensaje("No Tiene Ningun Descuento")
